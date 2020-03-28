723158570d48e1116fe159aa200a3950a032cea6
AnastasiyaKrasnova <anastasiya.krasnova2017@mail.ru>
2020-03-29 01:33:14 +0300

    Initial commit.

---
+++ Lab1/Lab01/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab01
+{
+    public partial class Form1 : Form
+    {
+        public enum TOperationType { add, edit, delete, browse };
+        public static List<MenuItem> MyMenu = new List<MenuItem>();
+        public static Form1 MainForm=null;
+        public Form1()
+        {
+            InitializeComponent();
+            MainForm = this;
+            ColumnHeader header1, header2, header3, header4;
+            header1 = new ColumnHeader();
+            header2 = new ColumnHeader();
+            header3 = new ColumnHeader();
+            header4 = new ColumnHeader();
+
+            header1.Text = "Item Name";
+            header1.Width = 400;
+            header2.Text = "Category";
+            header2.Width = 250;
+            header3.Text = "Calories";
+            header3.Width = 100;
+            header4.Text = "Price";
+            header4.Width = 150;
+
+            listView1.Columns.Add(header1);
+            listView1.Columns.Add(header2);
+            listView1.Columns.Add(header3);
+            listView1.Columns.Add(header4);
+
+            listView1.View = View.Details;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem != null)
+            {
+                string typename = String.Concat("Lab01.", comboBox1.SelectedItem.ToString());
+                Form2 AddForm = new Form2(typename, 0, TOperationType.add);
+                AddForm.Show();
+            }
+            else MessageBox.Show("Choose the category to add");
+        }
+
+        private void изменитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            foreach (int index in listView1.SelectedIndices)
+            {
+                string typename = String.Concat("Lab01.", listView1.Items[index].SubItems[1].Text);
+                Form2 AddForm = new Form2(typename, index, TOperationType.edit);
+                AddForm.Show();
+            }
+        }
+
+        private void удалитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            foreach (int index in listView1.SelectedIndices)
+            {
+                string typename = String.Concat("Lab01.", listView1.Items[index].SubItems[1].Text);
+                Form2 AddForm = new Form2(typename, index, TOperationType.delete);
+                AddForm.Show();
+            }
+        }
+
+        private void описаниеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            foreach (int index in listView1.SelectedIndices)
+            {
+                string typename = String.Concat("Lab01.", listView1.Items[index].SubItems[1].Text);
+                Form2 AddForm = new Form2(typename, index, TOperationType.browse);
+                AddForm.Show();
+            }
+        }
+    }
+}
---
+++ Lab1/Lab01/Form2.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab01
+{
+    public partial class Form2 : Form
+    {
+        delegate void ShowMethod(Form2 form);
+        public Type myType;
+        public int Num_of_Params;
+        Form1.TOperationType Op;
+        int fieldInd;
+        public Form2(string type, int index, Form1.TOperationType op)
+        {
+            InitializeComponent();
+            Op = op;
+            this.button1.Text = op.ToString();
+            fieldInd = index;
+            FormCreate.CreateComponents(type, this);
+            if (op != Form1.TOperationType.add)
+            {
+                FormCreate.InitComponents(index, myType, this);
+            }
+        }
+        static void AddItem(Type type,Form2 form)
+        {
+            object[] values = FormCreate.GetParams(type,form);
+            if (values != null)
+            {
+                MenuItem obj=FormCreate.CreateItem(values,type);
+                if (type.Name == "Combo")
+                {
+                    Combo combo = (Combo)obj;
+                    combo.Calories = combo.ComboBurger.Calories + combo.ComboDrink.Calories + combo.ComboGarneer.Calories;
+                    obj = combo;
+                }
+                if (!Form1.MyMenu.Exists(x => x.ItemName == obj.ItemName && x.Price == obj.Price && x.Calories == obj.Calories))
+                {
+                    Form1.MyMenu.Add(obj);
+                    ListViewItem lvi = new ListViewItem();
+                    ListViewItem.ListViewSubItem lvsi1 = new ListViewItem.ListViewSubItem();
+                    ListViewItem.ListViewSubItem lvsi2 = new ListViewItem.ListViewSubItem();
+                    ListViewItem.ListViewSubItem lvsi3 = new ListViewItem.ListViewSubItem();
+                    lvi.SubItems.Add(lvsi1);
+                    lvi.SubItems.Add(lvsi2);
+                    lvi.SubItems.Add(lvsi3);
+                    Form1.MainForm.listView1.Items.Add(lvi);
+                }
+            }  
+        }
+
+        static void EditItem(int index, Type type, Form2 form)
+        {
+            object[] values = FormCreate.GetParams(type, form);
+            if (values != null)
+            {
+                MenuItem obj = FormCreate.CreateItem(FormCreate.GetParams(type, form), type);
+                if (type.Name == "Combo")
+                {
+                    Combo combo = (Combo)obj;
+                    combo.Calories = combo.ComboBurger.Calories + combo.ComboDrink.Calories + combo.ComboGarneer.Calories;
+                    Form1.MyMenu.RemoveAt(index);
+                    Form1.MyMenu.Insert(index, obj);
+                }
+                else
+                {
+                    Form1.MyMenu.RemoveAt(index);
+                    Form1.MyMenu.Insert(index, obj);
+                }
+            }       
+        }
+        static void DeleteItem(int index)
+        {
+            Form1.MyMenu.RemoveAt(index);
+            Form1.MainForm.listView1.Items.RemoveAt(index);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            switch (Op)
+            {
+                case Form1.TOperationType.add:
+                    AddItem(myType,this);
+                    break;
+                case Form1.TOperationType.edit:
+                    EditItem(fieldInd, myType, this);
+                    break;
+                case Form1.TOperationType.delete:
+                    DeleteItem(fieldInd);
+                    break;
+            }
+            ShowMethod show = MyDelegetes.ShowList;
+            show.Invoke(this);
+            show += MyDelegetes.reNewComboBox;
+            if (MyDelegetes.ParentForm != null)
+                show.Invoke(MyDelegetes.ParentForm);
+            else
+                show.Invoke(this);
+            this.Close();
+        }
+
+        public static void But1_Click(object sender, EventArgs e)
+        {
+            string typename = String.Concat("Lab01.", ((sender as Button).Name).Remove(0, 5));
+            Form2 form = (Form2)(sender as Button).Parent;
+            string controlName = String.Concat("Comb1.", ((sender as Button).Name).Remove(0, 5));
+            Control[] controls = (form.Controls).Find(controlName, false);
+            MyDelegetes.ParentForm = (Form2)(sender as Button).Parent;//
+            Form2 AddForm = new Form2(typename, 0, Form1.TOperationType.add);
+            AddForm.Show();
+        }
+
+        public static void But2_Click(object sender, EventArgs e)
+        {
+            string typename = String.Concat("Lab01.", ((sender as Button).Name).Remove(0, 5));
+            Form2 form = (Form2)(sender as Button).Parent;
+            string controlName = String.Concat("Comb1.", ((sender as Button).Name).Remove(0, 5));
+            Control[] controls = (form.Controls).Find(controlName, false);
+            if ((controls[0] as ComboBox).SelectedItem != null)
+            {
+                int index = Form1.MyMenu.FindIndex(x => x.ItemName == (controls[0] as ComboBox).SelectedItem.ToString());
+                MyDelegetes.ParentForm = (Form2)(sender as Button).Parent;//
+                Form2 AddForm = new Form2(typename, index, Form1.TOperationType.edit);
+                AddForm.Show();
+            }
+        }
+        
+    }
+}
